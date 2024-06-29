b5734fe4e1587644a629d34155bd83face300d89
AlejoAcelas <alejoacelas@gmail.com>
2024-06-29 08:03:36 -0500

    add new changes

--- aws-nuke-cleanser/config/nuke_config_update.py
+++ aws-nuke-cleanser/config/nuke_config_update.py
@@ -14,21 +14,19 @@ GLOBAL_RESOURCE_EXCEPTIONS = [
     {"type": "regex", "value": ".*nuke-account-cleanser*.*"},
     {"type": "regex", "value": ".*securityhub*.*"},
     {"type": "regex", "value": ".*aws-prod*.*"},
+    {"property": "CreationDate", "type": "dateOlderThan", "value": "24h", "invert": "true"},
 ]
 
 class StackInfo:
 
-    def __init__(self, account, target_regions):
+    def __init__(self, account):
         self.session = boto3.Session(profile_name="default")
-        # Regions to be targeted set from the Stepfunctions/CodeBuild workflow
-        self.regions = target_regions
         self.resources = {}
         self.config = {}
         self.account = account
 
     def Populate(self):
-        self.UpdateCFNStackList()
-        self.BuildIamExclusionList(region) # Modify to pass self.regions
+        self.BuildIamExclusionList(self.regions) # Modify to pass self.regions
         self.OverrideDefaultConfig()
 
     def UpdateResourceName(self, resource):
@@ -37,34 +35,36 @@ class StackInfo:
         nuke_type = str.replace(nuke_type, "Config", "ConfigService", 1)
         return nuke_type
 
-    def BuildIamExclusionList(self, region):
+    def BuildIamExclusionList(self, regions):
         # This excludes and appends to the config IAMRole resources , the roles that are federated principals
         # You can add any other custom filterting logic based on regions for IAM/Global roles that should be excluded
-        iam_client = self.session.client("iam", region_name=region)
-        iam_paginator = iam_client.get_paginator("list_roles")
-        responses = iam_paginator.paginate()
-        for page in responses:
-            for role in page["Roles"]:
-                apd = role.get("AssumeRolePolicyDocument")
-                if apd is not None:
-                    for item in apd.get("Statement"):
-                        if item is not None:
-                            for principal in item.get("Principal"):
-                                if principal == "Federated":
-                                    if "IAMRole" in self.resources:
-                                        self.resources["IAMRole"].append(
-                                            role.get("RoleName")
-                                        )
-                                    else:
-                                        self.resources["IAMRole"] = [
-                                            role.get("RoleName")
-                                        ]
+        for region in regions:
+            iam_client = self.session.client("iam", region_name=region)
+            iam_paginator = iam_client.get_paginator("list_roles")
+            responses = iam_paginator.paginate()
+            for page in responses:
+                for role in page["Roles"]:
+                    apd = role.get("AssumeRolePolicyDocument")
+                    if apd is not None:
+                        for item in apd.get("Statement"):
+                            if item is not None:
+                                for principal in item.get("Principal"):
+                                    if principal == "Federated":
+                                        if "IAMRole" in self.resources:
+                                            self.resources["IAMRole"].append(
+                                                role.get("RoleName")
+                                            )
+                                        else:
+                                            self.resources["IAMRole"] = [
+                                                role.get("RoleName")
+                                            ]
 
     def OverrideDefaultConfig(self):
         # Open the nuke_generic_config.yaml and merge the captured resources/exclusions with it
         try:
             with open(r"nuke_generic_config.yaml") as config_file:
                 self.config = yaml.safe_load(config_file)
+                self.BuildIamExclusionList(self.config["regions"])
                 # Not all resources handled by the tool, but we will add them to the exclusion anyhow.
                 for resource in self.resources:
                     if resource in self.config["accounts"]["ACCOUNT"]["filters"]:
@@ -75,17 +75,23 @@ class StackInfo:
                         self.config["accounts"]["ACCOUNT"]["filters"][
                             resource
                         ] = self.resources[resource]
+                
                 self.config["accounts"][self.account] = copy.deepcopy(
                     self.config["accounts"]["ACCOUNT"]
                 )
                 if "ACCOUNT" in self.config["accounts"]:
                     self.config["accounts"].pop("ACCOUNT", None)
             # Global exclusions apply to every type of resource
-            for resource in self.config["accounts"][self.account]["filters"]:
+            for resource in self.config["resource-types"]['targets']:
                 for exception in GLOBAL_RESOURCE_EXCEPTIONS:
-                    self.config["accounts"][self.account]["filters"][resource].append(
-                        exception.copy()
-                    )
+                    if resource in self.config["accounts"][self.account]["filters"]:
+                        self.config["accounts"][self.account]["filters"][resource].append(
+                            exception.copy()
+                        )
+                    else:
+                        self.config["accounts"][self.account]["filters"][resource] = [
+                            exception.copy()
+                        ]
             config_file.close()
         except Exception as e:
             print("Failed merging nuke-config-test.yaml with error {}".format(e))
@@ -94,13 +100,10 @@ class StackInfo:
     def WriteConfig(self):
         # CodeBuild script updates the target region in the generic config and is validated here.
         try:
-            for region in self.config["regions"]:
-                local_config = stackInfo.config.copy()
-                local_config["regions"] = [region]
-                filename = "nuke_config_{}.yaml".format(region)
-                with open(filename, "w") as output_file:
-                    output = yaml.dump(local_config, output_file)
-                output_file.close()
+            filename = "nuke_config.yaml"
+            with open(filename, "w") as output_file:
+                output = yaml.dump(self.config, output_file)
+            output_file.close()
         except Exception as e:
             print("Failed opening nuke_config.yaml for writing with error {}".format(e))
 
@@ -108,9 +111,8 @@ class StackInfo:
 try:
     parser = argparse.ArgumentParser()
     parser.add_argument("--account", dest="account", help="Account to nuke") # Account and Region from StepFunctions - CodeBuild overridden params
-    parser.add_argument("--region", dest="region", help="Region to target for nuke")
     args = parser.parse_args()
-    if not args.account or not args.region:
+    if not args.account:
         parser.print_help()
         exit(1)
 except Exception as e:
@@ -119,6 +121,6 @@ except Exception as e:
 
 if __name__ == "__main__":
     print("Incoming Args: ", args)
-    stackInfo = StackInfo(args.account, [args.region])
-    stackInfo.Populate()
+    stackInfo = StackInfo(args.account)
+    stackInfo.OverrideDefaultConfig()
     stackInfo.WriteConfig()
